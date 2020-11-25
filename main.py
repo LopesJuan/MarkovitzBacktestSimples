16c0698d89ce9942025b33b9fd730457463956e5
JuanLopinhos <juanlopes@estudante.ufscar.br>
2020-11-25 16:52:31 -0300

    Falta adicionar o benchmark

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def efficient_frontier(mean_returns, cov_matrix, returns_range):
     return efficients
 
 
-def display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate):
+def display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate, retornos_finais):
     results, _ = random_portfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate)
 
     max_sharpe = max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate)
@@ -124,11 +124,15 @@ def display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, 
     print("Annualised Return:", round(rp, 2))
     print("Annualised Volatility:", round(sdp, 2))
     print(max_sharpe_allocation)
+    max_sharpe_allocation.to_csv("sharpe_allocation.csv")
     print("-" * 80)
     print("Minimum Volatility Portfolio Allocation\n")
     print("Annualised Return:", round(rp_min, 2))
     print("Annualised Volatility:", round(sdp_min, 2))
     print(min_vol_allocation)
+    min_vol_allocation.to_csv("min_vol_allocation.csv")
+
+
 
     plt.figure(figsize=(10, 7))
     plt.scatter(results[0, :], results[1, :], c=results[2, :], cmap='YlGnBu', marker='o', s=10, alpha=0.3)
@@ -149,19 +153,25 @@ def display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, 
 
 
 
-tickers = ["MGLU3.SA", "ITUB4.SA", "BBDC3.SA", "BBDC4.SA", "JBSS3.SA"]
-start = "2019-01-01"
+tickers = ["MGLU3.SA", "ITUB4.SA", "BBDC3.SA", "BBDC4.SA", "JBSS3.SA", "ITUB3.SA"]
+# tickers = ["ABEV3.SA", "ALPA4.SA", "ALSO3.SA", "AMAR3.SA", "AZUL4.SA", "B3SA3.SA", "BBAS3.SA", "BBDC3.SA", "BBDC4.SA", "BBSE3.SA", "BEEF3.SA", "BIDI4.SA", "BPAC11.SA", "BPAN4.SA", "BRAP4.SA", "BRDT3.SA", "BRFS3.SA", "BRKM5.SA", "BRML3.SA", "BTOW3.SA", "CCRO3.SA", "CESP6.SA", "CIEL3.SA", "CMIG4.SA", "CNTO3.SA", "COGN3.SA", "CPFE3.SA", "CPLE6.SA", "CRFB3.SA", "CSAN3.SA", "CSMG3.SA", "CSNA3.SA", "CVCB3.SA", "CYRE3.SA", "DTEX3.SA", "ECOR3.SA", "EGIE3.SA", "ELET3.SA", "ELET6.SA", "EMBR3.SA", "ENBR3.SA", "ENEV3.SA", "ENGI11.SA", "EQTL3.SA", "EZTC3.SA", "FLRY3.SA", "GGBR4.SA", "GNDI3.SA", "GOAU4.SA", "GOLL4.SA", "HAPV3.SA", "HGTX3.SA", "HYPE3.SA", "IGTA3.SA", "IRBR3.SA", "ITSA4.SA", "ITUB4.SA", "JBSS3.SA", "JHSF3.SA", "KLBN11.SA", "LAME3.SA", "LAME4.SA", "LCAM3.SA", "LIGT3.SA", "LINX3.SA", "LREN3.SA", "MDIA3.SA", "MGLU3.SA", "MOVI3.SA", "MRFG3.SA", "MRVE3.SA", "MULT3.SA", "NEOE3.SA", "NTCO3.SA", "PCAR3.SA", "PETR3.SA", "PETR4.SA", "PRIO3.SA", "PSSA3.SA", "QUAL3.SA", "RADL3.SA", "RAIL3.SA", "RAPT4.SA", "RENT3.SA", "SANB11.SA", "SAPR11.SA", "SBSP3.SA", "SULA11.SA", "SUZB3.SA", "TAEE11.SA", "TIMS3.SA", "TOTS3.SA", "TRPL4.SA", "UGPA3.SA", "USIM5.SA", "VALE3.SA", "VIVT3.SA", "VVAR3.SA", "WEGE3.SA", "YDUQ3.SA"]
+
+start = "2015-01-01"
 end = "2020-12-31"
+
 feedback = []
 
 df = prices(tickers, start, end)
-df_log_ret = np.log(df/df.shift(-1))
+# df_log_ret = np.log(df/df.shift(-1))
 
-returns = df.pct_change()
+retornos_finais = df.loc[datetime.date(year=2017,month=1,day=1):datetime.date(year=2020,month=12,day=31)].pct_change()
+
+returns = df.loc[datetime.date(year=2015,month=1,day=1):datetime.date(year=2016,month=12,day=31)].pct_change()
+
 table = df
 mean_returns = returns.mean()
 cov_matrix = returns.cov()
 num_portfolios = 50000
 risk_free_rate = 0.02
 
-display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate)
+display_calculated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate, retornos_finais)
